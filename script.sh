9323c8ba3555432d0f2c47c8bb4005e69227f1fe
AhmedAbouelkher <am303737@gmail.com>
2020-04-17 21:12:48 +0200

    bug fixes

--- script.sh
+++ script.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/bash
 
 #flags
-while getopts m: aflag; do
+while getopts m:l:u: aflag; do
 		case $aflag in
 				m) COMMIT=$OPTARG;;
+				l) REPO=$OPTARG;;
+				u) UPNREPO=$OPTARG;;
 		esac
 done
 
-git add . && git commit -m "${COMMIT}" && git push origin master
+if [ ${UPNREPO}]
+then
+    echo 'Uploading'
+else
+    git add . && git commit -m "${COMMIT}" && git push origin master
+fi
